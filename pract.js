1407d84ef8df231fc97de17e0d2e02d25765671f
gad mohamed <gad.mohamed184@yahoo.com>
2019-01-02 01:09:35 +0200

    2

--- pract.js
+++ pract.js
@@ -43,7 +43,7 @@ var g =
   process:function()
   {
       return function () {
-          console.log(this.number);
+          console.log( this.number);
       }
   }
 }
@@ -55,6 +55,60 @@ g.process()(); // ()() this mean fire procss fun which return functin then fire 
 var g =
 {
    number:115,
-   process: function() {return () => console.log(this.number); }
+   process: function() {return () => console.log( this.number); }
 }
 g.process()(); //115
+
+
+//Defult Function Parameter
+
+//In es5
+var d = function (product) {
+    if(product === undefined)
+    {
+        product = 200;
+    }
+    console.log(product);
+}
+d(); //200
+d(10); // 10
+
+//defult parameter In ES6
+var dd = function (product = 200) {
+    console.log(product);
+}
+dd(); //200
+dd(10); // 10
+
+
+var d = function (product = 300 , price = 50) {
+    console.log(product);
+    console.log(price);
+}
+d(); //300 , 50
+d(undefined,10); // 300,10
+
+// Reset Operator
+
+var rest = function (product , ...categry) {
+    console.log(categry[0]);
+}
+rest(1,2,3,4); // 2,3,4 pass as element in arry category 
+rest(1)        //[] Empty Arry 
+
+
+// Spread Operator
+
+var spread = [1,2,3,5];
+console.log(Math.max(...spread)); // 5  
+
+// max function  not accept arry but number separated by , spread operator do this with function
+
+
+
+
+
+
+
+
+
